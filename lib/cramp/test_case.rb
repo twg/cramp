6c97a9c923b4087ef78907a743a2afe72e824564
Oleg <oleg@theworkinggroup.ca>
2011-11-29 17:11:18 -0500

    adding all http verbs to the TestCase

--- lib/cramp/test_case.rb
+++ lib/cramp/test_case.rb
@@ -3,40 +3,52 @@ require 'active_support/test_case'
 
 module Cramp
   class TestCase < ::ActiveSupport::TestCase
-
+    
     setup :create_request
 
     def create_request
       @request = Rack::MockRequest.new(app)
     end
-
-    def get(path, options = {}, headers = {}, &block)
+    
+    %w(get post put delete).each do |verb|
+      define_method verb do |*args, &block|
+        request(verb, *args, &block)
+      end
+      define_method "#{verb}_body" do |*args, &block|
+        request_body(verb, *args, &block)
+      end
+      define_method "#{verb}_body_chunks" do |*args, &block|
+        request_body_chunks(verb, *args, &block)
+      end
+    end
+    
+    def request(method, path, options = {}, headers = {}, &block)
       callback = options.delete(:callback) || block
       headers = headers.merge('async.callback' => callback)
 
       EM.run do
-        catch(:async) { @request.get(path, headers) }
+        catch(:async) { @request.request(method, path, headers) }
       end
     end
-
-    def get_body(path, options = {}, headers = {}, &block)
+    
+    def request_body(method, path, options = {}, headers = {}, &block)
       callback = options.delete(:callback) || block
       response_callback = proc {|response| response[-1].each {|chunk| callback.call(chunk) } }
       headers = headers.merge('async.callback' => response_callback)
 
       EM.run do
-        catch(:async) { @request.get(path, headers) }
+        catch(:async) { @request.request(method, path, headers) }
       end
     end
 
-    def get_body_chunks(path, options = {}, headers = {}, &block)
+    def request_body_chunks(method, path, options = {}, headers = {}, &block)
       callback = options.delete(:callback) || block
       count = options.delete(:count) || 1
 
       stopping = false
       chunks = []
 
-      get_body(path, options, headers) do |body_chunk|
+      request_body(method, path, options, headers) do |body_chunk|
         chunks << body_chunk unless stopping
 
         if chunks.count >= count
